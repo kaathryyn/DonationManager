ffb25371aaf84d7030009ff7aa14737eaa0888c3
Piyush Vats <piyushvats@gmail.com>
2019-04-15 18:32:24 +1000

    Added toolbar
    -Added toolbar
    -Setup menu items

--- app/src/main/java/com/example/donationmanager/HomeActivity.java
+++ app/src/main/java/com/example/donationmanager/HomeActivity.java
@@ -1,8 +1,12 @@
 package com.example.donationmanager;
 
 import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,13 +18,14 @@ import org.w3c.dom.Text;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
+
+    private DrawerLayout drawer;
     //firebase auth object
-    private FirebaseAuth firebaseAuth;
+    //private FirebaseAuth firebaseAuth;
 
     //activity elements
-    private TextView textViewUserEmail;
-    private Button buttonLogout;
-
+    //private TextView textViewUserEmail;
+    //private Button buttonLogout;
 
 
     @Override
@@ -28,13 +33,22 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
         //initialise firebase object
 
-        firebaseAuth = FirebaseAuth.getInstance();
+        //firebaseAuth = FirebaseAuth.getInstance();
 
         //
 
-        if(firebaseAuth.getCurrentUser() == null) {
+       /* if (firebaseAuth.getCurrentUser() == null) {
             //close activity
             finish();
             //go back to login
@@ -50,23 +64,33 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         buttonLogout = (Button) findViewById(R.id.buttonLogout);
 
         //display account email
-        textViewUserEmail.setText("Welcome"+user.getEmail());
+        textViewUserEmail.setText("Welcome" + user.getEmail());
 
         //add click listener on logout button
-        buttonLogout.setOnClickListener(this);
+        buttonLogout.setOnClickListener(this);*/
     }
 
+    @Override
+    public void onBackPressed() {
+
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
 
     @Override
     public void onClick(View view) {
 
         //if user logs out
-        if (view == buttonLogout) {
+
+        /*if (view == buttonLogout) {
             firebaseAuth.signOut();
             finish();
             startActivity(new Intent(this, LoginActivity.class));
-        }
+        }*/
 
     }
 }

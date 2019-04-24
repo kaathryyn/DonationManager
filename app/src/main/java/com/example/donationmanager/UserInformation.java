41ea3830aa12218ec7bc8e72ef4e864b52dc8907
pseudoptics <bananamagic.com@gmail.com>
2019-04-24 12:00:37 +1000

    Updated UserBooking, UserInformation Fields

--- app/src/main/java/com/example/donationmanager/UserInformation.java
+++ app/src/main/java/com/example/donationmanager/UserInformation.java
@@ -5,10 +5,12 @@ public class UserInformation {
     public String name;
     public String address;
     public String accountType;
+    public int phoneNumber;
 
-    public UserInformation(String name, String address, String accountType) {
+    public UserInformation(String name, String address, String accountType, int phoneNumber) {
         this.name = name;
         this.address = address;
         this.accountType = accountType;
+        this.phoneNumber = phoneNumber;
     }
 }

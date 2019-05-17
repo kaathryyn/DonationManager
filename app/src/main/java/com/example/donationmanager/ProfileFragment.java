2ba6ee7698a5fc47c2d2a24ff16f9ff2dc49d66d
Piyush Vats <piyushvats@gmail.com>
2019-05-17 10:47:04 +1000

    hide spinners

--- app/src/main/java/com/example/donationmanager/ProfileFragment.java
+++ app/src/main/java/com/example/donationmanager/ProfileFragment.java
@@ -35,7 +35,7 @@ public class ProfileFragment extends Fragment implements AdapterView.OnItemSelec
 
     private EditText editCharityName, editFirstName, editLastName, editAddress, editCity, editPostcode, editState, editPhoneNumber;
     private int openHour, closeHour;
-    private boolean mon, tue, wed, thu, fri, sat, sun;
+    private CheckBox mon, tue, wed, thu, fri, sat, sun;
     private Button buttonSave;
     private String accountType;
     private TextView tvOpenHours, tvCloseHours, tvOpenDays;
@@ -71,6 +71,17 @@ public class ProfileFragment extends Fragment implements AdapterView.OnItemSelec
         spinner1.setAdapter(adapter1);
         spinner1.setOnItemSelectedListener(this);
 
+        //initialise days checkboes
+
+        CheckBox mon = (CheckBox) v.findViewById(R.id.checkbox_monday);
+        CheckBox tue = (CheckBox) v.findViewById(R.id.checkbox_tuesday);
+        CheckBox wed = (CheckBox) v.findViewById(R.id.checkbox_wednesday);
+        CheckBox thu = (CheckBox) v.findViewById(R.id.checkbox_thursday);
+        CheckBox fri = (CheckBox) v.findViewById(R.id.checkbox_friday);
+        CheckBox sat = (CheckBox) v.findViewById(R.id.checkbox_saturday);
+        CheckBox sun = (CheckBox) v.findViewById(R.id.checkbox_sunday);
+
+
         accountType = spinner1.getSelectedItem().toString();
 
 
@@ -81,6 +92,24 @@ public class ProfileFragment extends Fragment implements AdapterView.OnItemSelec
         spinner2.setAdapter(adapter2);
         spinner2.setOnItemSelectedListener(this);
 
+        //initialise opening hours spinner
+
+        Spinner  spinner3 = (Spinner) v.findViewById(R.id.openHourSpinner);
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(v.getContext(),R.array.hours,android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner3.setAdapter(adapter3);
+        spinner3.setOnItemSelectedListener(this);
+
+        //initialise closing hours spinner
+
+        Spinner  spinner4 = (Spinner) v.findViewById(R.id.closeHourSpinner);
+        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(v.getContext(),R.array.hours,android.R.layout.simple_spinner_item);
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner4.setAdapter(adapter3);
+        spinner4.setOnItemSelectedListener(this);
+
+
+
 
 
         //hide or show fields based on account type
@@ -90,6 +119,17 @@ public class ProfileFragment extends Fragment implements AdapterView.OnItemSelec
             tvOpenDays.setVisibility(View.GONE);
             tvCloseHours.setVisibility(View.GONE);
             tvOpenHours.setVisibility(View.GONE);
+            spinner4.setVisibility(View.GONE);
+            spinner3.setVisibility(View.GONE);
+            mon.setVisibility(View.GONE);
+            tue.setVisibility(View.GONE);
+            wed.setVisibility(View.GONE);
+            thu.setVisibility(View.GONE);
+            fri.setVisibility(View.GONE);
+            sat.setVisibility(View.GONE);
+            sun.setVisibility(View.GONE);
+
+
 
 
 
@@ -97,6 +137,44 @@ public class ProfileFragment extends Fragment implements AdapterView.OnItemSelec
         else {
             editFirstName.setVisibility(v.GONE);
             editLastName.setVisibility(v.GONE);
+
+        }
+
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            if(parent.getSelectedItemPosition() == 0) {
+                editCharityName.setVisibility(View.GONE);
+                editFirstName.setVisibility(View.VISIBLE);
+                editLastName.setVisibility(View.VISIBLE);
+            }
+
+            else if (parent.getSelectedItemPosition() == 1) {
+                editFirstName.setVisibility(View.GONE);
+                editLastName.setVisibility(View.GONE);
+                editCharityName.setVisibility(View.VISIBLE);
+                tvOpenDays.setVisibility(View.VISIBLE);
+                tvCloseHours.setVisibility(View.VISIBLE);
+                tvOpenHours.setVisibility(View.VISIBLE);
+                spinner4.setVisibility(View.GONE);
+                spinner3.setVisibility(View.GONE);
+                mon.setVisibility(View.GONE);
+                tue.setVisibility(View.GONE);
+                wed.setVisibility(View.GONE);
+                thu.setVisibility(View.GONE);
+                fri.setVisibility(View.GONE);
+                sat.setVisibility(View.GONE);
+                sun.setVisibility(View.GONE);
+
+            }
+
+            String text = parent.getItemAtPosition(position).toString();
+            Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
 
         }
 
@@ -127,29 +205,7 @@ public class ProfileFragment extends Fragment implements AdapterView.OnItemSelec
         Toast.makeText(getContext(), "saved", Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-        if(parent.getSelectedItemPosition() == 0) {
-            editCharityName.setVisibility(View.GONE);
-            editFirstName.setVisibility(View.VISIBLE);
-            editLastName.setVisibility(View.VISIBLE);
-        }
-
-        else if (parent.getSelectedItemPosition() == 1) {
-            editFirstName.setVisibility(View.GONE);
-            editLastName.setVisibility(View.GONE);
-            editCharityName.setVisibility(View.VISIBLE);
-        }
-
-        String text = parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
 
     @Override
     public void onClick(View v) {
